22a16b0098370b075660e63ed9eed0bb4ece50f6
Francesco <francescococlite@gmail.com>
2021-06-22 15:18:20 +0200

    add click function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
 var app = new Vue({
     el: '#app',
     data: {
-      
+        counter: 0,
+        photo: [
+            'img/img1.jpg',
+            'img/img2.jpg',
+            'img/img3.jpg',
+            'img/img4.jpg',
+        ]
+    },
+    methods:{
+        succ(){
+            if (this.counter == this.photo.length-1){
+                this.counter = 0;
+                
+            }
+            else{
+                this.counter++;
+            }
+        },
+        prec(){
+            if (this.counter == 0){
+                this.counter = this.photo.length-1;
+            }
+            else{
+                this.counter--;
+            }
+        }
     }
   })
